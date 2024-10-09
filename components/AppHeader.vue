0a6d06193d93d60196884064c7f9d1111a112942
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-09 06:11:23 +0300

    feat: init ui

---
+++ components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import { Moon, Sun, Zap } from 'lucide-vue-next';
+
+const colorMode = useColorMode();
+</script>
+
+<template>
+    <nav class="flex justify-between items-center p-2">
+        <div class="ml-4">
+            <Zap class="inline-block h-4 w-4" /> FLASHY
+        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger as-child>
+                <Button variant="outline">
+                    <Moon
+                        class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                    <Sun
+                        class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                    <span class="sr-only">Toggle theme</span>
+                </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+                <DropdownMenuItem @click="colorMode.preference = 'light'">
+                    Light
+                </DropdownMenuItem>
+                <DropdownMenuItem @click="colorMode.preference = 'dark'">
+                    Dark
+                </DropdownMenuItem>
+                <DropdownMenuItem @click="colorMode.preference = 'system'">
+                    System
+                </DropdownMenuItem>
+            </DropdownMenuContent>
+        </DropdownMenu>
+    </nav>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,6 @@
+<template>
+    <div class="flex flex-col w-screen h-screen">
+        <AppHeader />
+        <slot />
+    </div>
+</template>
