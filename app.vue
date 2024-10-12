2264ac386135923072a0bb45a05ace65dad2c91b
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-12 06:10:28 +0300

    feat: add toaster

--- app.vue
+++ app.vue
@@ -1,6 +1,11 @@
+<script lang="ts" setup>
+import Toaster from './components/ui/toast/Toaster.vue';
+</script>
+
 <template>
   <NuxtLayout>
     <NuxtRouteAnnouncer />
     <NuxtPage />
   </NuxtLayout>
+  <Toaster />
 </template>
