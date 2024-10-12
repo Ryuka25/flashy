a612d9058ab88d8c8cc33ef0e7a7cba631e6c096
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-12 09:38:32 +0300

    feat: add-tag-manager

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,6 +8,7 @@ export default defineNuxtConfig({
     '@nuxtjs/color-mode',
     '@nuxt/fonts',
     '@nuxt/icon',
+    'nuxt-gtag',
   ],
   colorMode: {
     classSuffix: '',
@@ -22,5 +23,8 @@ export default defineNuxtConfig({
      * @default "./components/ui"
      */
     componentDir: './components/ui',
+  },
+  gtag: {
+    // enabled: process.env.NODE_ENV === 'production',
   },
 })
