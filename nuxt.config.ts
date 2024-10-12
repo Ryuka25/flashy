7cba856ee019ff03818bfcb638ac05f670637757
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-12 09:00:22 +0300

    feat: improve seo

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
+  app: {
+    head: {
+      htmlAttrs: {
+        lang: 'en',
+      },
+      link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }],
+    },
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxtjs/tailwindcss',
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { useToast } from '@/components/ui/toast/use-toast'
 
+import { APP_DESCRIPTION } from '~/lib/consts';
 import shortcuts from '~/data/shortcuts.json';
 
 const { toast } = useToast();
@@ -16,12 +17,10 @@ const onGetStartedClick = () => {
 <template>
     <div class="flex flex-col lg:flex-row items-center justify-around grow max-w-7xl mx-auto px-4">
         <div class="lg:block lg:text-left text-center">
-            <h2 class="text-5xl lg:text-7xl font-bold"><span class="underline">Flashy</span> the
-                way you code</h2>
-            <p class="hidden lg:block my-8">
-                Unlock your full coding potential and master essential keyboard shortcuts for
-                your favorite IDE — boost productivity and supercharge your workflow 🚀
-            </p>
+            <h1 class="text-5xl lg:text-7xl font-bold"><span class="underline">Flashy</span> the
+                way you code</h1>
+            <!-- Any change to description should be reflected in App.vue setup -->
+            <p class="hidden lg:block my-8">{{ APP_DESCRIPTION }}</p>
             <div class="flex justify-center lg:justify-start">
                 <Button @click="onGetStartedClick" class="my-4 text-right">
                     <Icon name="lucide:rocket" class="h-4 w-4 mr-4" />
