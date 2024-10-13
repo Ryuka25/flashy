0c9cee1550c7e12134d77168c18752fbb18c3c21
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-13 17:40:12 +0300

    fix: grouping logic for shortcuts

--- components/ShortcutGroup.vue
+++ components/ShortcutGroup.vue
@@ -25,14 +25,24 @@ let splitKeyboardGroupString = function (groupString: string): string[] {
     // Replace placeholder back to original character
     return parts.map((key) => key.replace(ESCAPED_PLUS_PLACEHOLDER, "+"));
 };
+
+/**
+ * Returns an array of words from a given string.
+ *
+ * @param {string} testString - The string to get the words from.
+ * @returns {string[]} - An array of words in the given string.
+ */
+function getWords(testString: string): string[] {
+    return testString.trim().split(/\s+/);
+}
 </script>
 
 <template>
     <div class="flex justify-center gap-2 border-2 rounded-xl p-2 border-b-4 dark:border-white"
-        v-if="keyboardGroup.length > 1">
+        v-if="getWords(keyboardGroup).length > 1">
         <ShortcutKey v-for="keyboardKey in splitKeyboardGroupString(keyboardGroup)" :keyboardKey="keyboardKey" />
     </div>
     <div v-else class="flex justify-center p-2">
-        <ShortcutKey :keyboardKey="keyboardGroup[0]" />
+        <ShortcutKey :keyboardKey="getWords(keyboardGroup)[0]" />
     </div>
 </template>
