648e888a0ecb6cc9d258dedaeae46e58f943ef93
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-15 15:09:56 +0300

    refact: purify default layout

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -7,26 +7,24 @@ const toogleColorMode = () => {
 </script>
 
 <template>
-    <header>
-        <div class="mx-auto py-2 px-4 max-w-7xl flex items-center justify-between">
-            <NuxtLink to="/">
-                <AppName />
-            </NuxtLink>
-            <div class="flex gap-2">
-                <Button variant="ghost" size="icon" @click="toogleColorMode">
-                    <Icon name="lucide:moon"
-                        class="h-4 w-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-                    <Icon name="lucide:sun"
-                        class="absolute h-4 w-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-                    <span class="sr-only">Toggle theme</span>
-                </Button>
-                <Button variant="ghost" size="icon" as-child>
-                    <NuxtLink to="https://github.com/Ryuka25/flashy" target="_blank">
-                        <Icon name="simple-icons:github" class="h-4 w-4" />
-                        <span class="sr-only">Show on GitHub</span>
-                    </NuxtLink>
-                </Button>
-            </div>
+    <header class="flex items-center justify-between">
+        <NuxtLink to="/">
+            <AppName />
+        </NuxtLink>
+        <div class="flex gap-2">
+            <Button variant="ghost" size="icon" @click="toogleColorMode">
+                <Icon name="lucide:moon"
+                    class="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+                <Icon name="lucide:sun"
+                    class="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+                <span class="sr-only">Toggle theme</span>
+            </Button>
+            <Button variant="ghost" size="icon" as-child>
+                <NuxtLink to="https://github.com/Ryuka25/flashy" target="_blank">
+                    <Icon name="simple-icons:github" class="h-4 w-4" />
+                    <span class="sr-only">Show on GitHub</span>
+                </NuxtLink>
+            </Button>
         </div>
     </header>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -15,11 +15,10 @@ const onGetStartedClick = () => {
 </script>
 
 <template>
-    <div class="flex flex-col lg:flex-row items-center justify-around grow max-w-7xl mx-auto px-4">
+    <main class="flex-1 flex flex-col lg:flex-row items-center justify-around">
         <div class="lg:block lg:text-left text-center">
             <h1 class="text-5xl lg:text-7xl font-bold"><span class="underline">Flashy</span> the
                 way you code</h1>
-            <!-- Any change to description should be reflected in App.vue setup -->
             <p class="hidden lg:block my-8">{{ APP_DESCRIPTION }}</p>
             <div class="flex justify-center lg:justify-start">
                 <Button @click="onGetStartedClick" class="my-4 text-right">
@@ -37,5 +36,5 @@ const onGetStartedClick = () => {
         <div class="flex flex-col items-center w-full">
             <ShortcutCarousel id="shortcuts" v-if="shortcuts && shortcuts.length > 0" :shortcuts="shortcuts" />
         </div>
-    </div>
+    </main>
 </template>
