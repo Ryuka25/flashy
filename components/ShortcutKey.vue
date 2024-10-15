a5ee218e311544504825c90508142d67b4128e86
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-15 16:03:01 +0300

    feat: add support for long keyboard name on small devices

--- components/ShortcutKey.vue
+++ components/ShortcutKey.vue
@@ -2,10 +2,29 @@
 defineProps<{
     keyboardKey: string;
 }>()
+
+/**
+ * Returns the class name for a given keyboard key based on its length.
+ * 
+ * @param {string} key - The keyboard key to get the class name for.
+ * @returns {string} The class name for the given keyboard key.
+ */
+const getKeyClass = (key: string): string => {
+    const length = key.length;
+    if (length === 1) {
+        return 'aspect-square';
+    }
+    if (length > 5) {
+        // e.g: '← backspace'
+        return 'text-xs';
+    }
+    return '';
+};
+
 </script>
 
 <template>
-    <kbd :class="keyboardKey.length == 1 ? 'aspect-square' : ''"
+    <kbd :class="getKeyClass(keyboardKey)"
         class="dark:border-white uppercase border-2 border-b-4 hover:cursor-pointer hover:border-b-2 rounded-lg px-4 h-14 flex justify-center items-center text-lg">
         {{ keyboardKey }}
     </kbd>
