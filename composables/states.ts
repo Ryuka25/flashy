621e2fb10cd621ec978caf0dc510f26623e2f637
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-16 07:39:15 +0300

    feat: add stop button for autoplay

---
+++ composables/states.ts
@@ -0,0 +1,4 @@
+import type { CarouselApi } from "~/components/ui/carousel"
+
+export const useShortcutAutoplay = () => useState('shortcutAutoplay', () => true);
+export const useShortcutCarouselEmblaApi = () => useState<CarouselApi | null>('shortcutCarouselEmblaApi', () => null);
