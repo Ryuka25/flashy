d168506d2501ac116c03c8ebf40fe8028d579b19
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-17 02:05:56 +0300

    feat: ad og meta for seo

--- app.vue
+++ app.vue
@@ -1,13 +1,18 @@
 <script lang="ts" setup>
-import Toaster from './components/ui/toast/Toaster.vue';
-import { APP_DESCRIPTION } from './lib/consts';
+import Toaster from "./components/ui/toast/Toaster.vue";
+import { APP_DESCRIPTION } from "./lib/consts";
 
-useHead({
-  title: 'Flashy',
-  meta: [
-    { name: 'description', content: APP_DESCRIPTION }
-  ],
-})
+useSeoMeta({
+  title: "Flashy",
+  ogTitle: "Master your favoride IDE keyboard shortcuts - Flashy",
+  twitterTitle: "Master your favoride IDE keyboard shortcuts - Flashy",
+  description: APP_DESCRIPTION,
+  ogDescription: APP_DESCRIPTION,
+  twitterDescription: APP_DESCRIPTION,
+  ogImage: "https://flashy-dev.vercel.app/images/flashy-preview.png",
+  twitterImage: "https://flashy-dev.vercel.app/images/flashy-preview.png",
+  twitterCard: "summary_large_image",
+});
 </script>
 
 <template>
