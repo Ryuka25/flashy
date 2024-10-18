5bf0cf9eeb97f9887d81fc7424f52c36f2a5ea10
Lovanirina R <lovanirina.ran@gmail.com>
2024-10-18 18:47:05 +0300

    feat(layout): update to make more standard

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="container mx-auto flex min-h-screen flex-col">
+  <div
+    class="mx-auto flex min-h-screen max-w-7xl flex-col px-4 sm:px-6 lg:px-8"
+  >
     <!-- Header -->
     <AppHeader class="py-2" />
     <!-- Body (Content area) -->
