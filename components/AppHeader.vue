d8bd6cede68666ce09e8c67dc2d55d6e14d9536d
Lovanirina R <lovanirina.ran@gmail.com>
2024-11-24 10:13:01 +0300

    feat: change mobile header navigation

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -4,42 +4,99 @@ const colorMode = useColorMode();
 const toogleColorMode = () => {
   colorMode.preference = colorMode.preference === "light" ? "dark" : "light";
 };
+
+import { Menu, Home, ContactRound } from "lucide-vue-next";
+
+const isOpen = useState(() => false);
+
+const closeMenu = () => {
+  isOpen.value = false;
+};
+
+const navigation = [
+  {
+    label: "Home",
+    icon: Home,
+    to: "/",
+  },
+  {
+    label: "Contact",
+    icon: ContactRound,
+    to: "/contact",
+  },
+];
 </script>
 
 <template>
-  <header class="flex items-center justify-between">
-    <div class="flex items-center">
-      <NuxtLink to="/">
+  <header>
+    <div class="flex items-center justify-between">
+      <NuxtLink to="/" class="flex-1">
         <AppName />
       </NuxtLink>
-      <div class="ml-8">
-        <NuxtLink
-          to="/contact"
-          class="opacity-80 hover:opacity-100"
-          active-class="!opacity-100"
-        >
-          Contact
-        </NuxtLink>
+      <!-- Desktop navigation -->
+      <nav class="hidden md:block">
+        <ul class="flex gap-4">
+          <li v-for="link in navigation">
+            <NuxtLink
+              :to="link.to"
+              class="text-foreground/80 hover:text-foreground"
+              active-class="!text-foreground"
+            >
+              {{ link.label }}
+            </NuxtLink>
+          </li>
+        </ul>
+      </nav>
+      <!-- /Desktop navigation -->
+      <div class="flex flex-1 items-center justify-end gap-1">
+        <Button variant="ghost" size="icon" @click="toogleColorMode">
+          <Icon
+            name="lucide:moon"
+            class="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+          />
+          <Icon
+            name="lucide:sun"
+            class="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+          />
+          <span class="sr-only">Toggle theme</span>
+        </Button>
+        <Button variant="ghost" size="icon" as-child>
+          <NuxtLink to="https://github.com/Ryuka25/flashy" target="_blank">
+            <Icon name="simple-icons:github" class="h-4 w-4" />
+            <span class="sr-only">Show on GitHub</span>
+          </NuxtLink>
+        </Button>
+        <Sheet :open="isOpen" @update:open="(value) => (isOpen = value)">
+          <SheetTrigger as-child>
+            <Button variant="outline" size="icon" class="md:hidden">
+              <Menu :class="['size-4 transition-all']" />
+              <span class="sr-only">Toggle menu</span>
+            </Button>
+          </SheetTrigger>
+          <SheetContent side="left">
+            <NuxtLink to="/" class="flex-1" @click="closeMenu">
+              <AppName />
+            </NuxtLink>
+            <!-- Mobile Navigation -->
+            <nav class="w-full bg-background">
+              <ul class="flex flex-col justify-center gap-4 p-4">
+                <li v-for="link in navigation">
+                  <NuxtLink
+                    :to="link.to"
+                    class="flex items-center gap-2 text-foreground/80 hover:text-foreground"
+                    active-class="!text-foreground"
+                    @click="closeMenu"
+                  >
+                    <component :is="link.icon" class="size-5" />
+                    {{ link.label }}
+                  </NuxtLink>
+                </li>
+              </ul>
+            </nav>
+            <!--/Mobile Navigation -->
+          </SheetContent>
+        </Sheet>
       </div>
-    </div>
-    <div class="flex gap-2">
-      <Button variant="ghost" size="icon" @click="toogleColorMode">
-        <Icon
-          name="lucide:moon"
-          class="size-4 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
-        />
-        <Icon
-          name="lucide:sun"
-          class="absolute size-4 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
-        />
-        <span class="sr-only">Toggle theme</span>
-      </Button>
-      <Button variant="ghost" size="icon" as-child>
-        <NuxtLink to="https://github.com/Ryuka25/flashy" target="_blank">
-          <Icon name="simple-icons:github" class="h-4 w-4" />
-          <span class="sr-only">Show on GitHub</span>
-        </NuxtLink>
-      </Button>
     </div>
   </header>
 </template>
